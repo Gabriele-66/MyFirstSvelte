b14b71f08b6b367f5dc97027a188378285a0355e
Gabriele-66 <gabrielerecchia70@gmail.com>
2022-02-11 14:51:59 +0100

    login: form without form

--- src/component/Login/Login.svelte
+++ src/component/Login/Login.svelte
@@ -1,9 +1,22 @@
 <script>
     let usrNm = "";
     let pwd = "";
-    let submitted = false;
+    let sub = false;
+    let usrNmC = false;
+    let pwdC = false;
     function handleSubmit() {
-		alert(`UserName: ${usrNm}, Password: ${pwd}`);
+        if(usrNm!="" && pwd!=""){
+            alert(`UserName: ${usrNm}, Password: ${pwd}`);
+            usrNmC=false;
+            pwdC=false;
+        }
+        sub = true;
+
+        if(usrNm=="")   usrNmC=true;
+        else usrNmC=false;
+
+        if(pwd=="")   pwdC=true;
+        else pwdC=false;
 	}
 </script>
 
@@ -12,20 +25,22 @@
     <div class="container">
         <div id="login-row" class="row justify-content-center align-items-center">
             <div id="login-box" class="col-">
-                <form id="login-form" class="form" action="" method="post" class:submitted on:submit|preventDefault={handleSubmit}>
+                <div id="login-form" class="form" class:sub action="" method="post">
                     <h3 class="text-center text-info">Login to Your Account</h3>
                     <div class="form-group">
                         <label for="username" class="text-info">Username:</label>
-                        <input type="text" name="username" id="username" class="form-control" bind:value={usrNm} required><br>
+                        {#if usrNmC}<p>USERNAME OBBLIGATORIO</p>{/if}
+                        <input type="text" name="username" id="username" class="form-control" bind:value={usrNm} required>
                     </div>
                     <div class="form-group">
                         <label for="password" class="text-info">Password:</label>
-                        <input type="text" name="password" id="password" class="form-control" bind:value={pwd} required><br>
+                        {#if pwdC}<p>PASSWORD OBBLIGATORIA</p>{/if}
+                        <input type="text" name="password" id="password" class="form-control" bind:value={pwd} required>
                     </div>
                     <div class="form-group" style="text-align:center">
-                        <button type="submit" name="submit" class="btn btn-info" value="submit" on:click={() => submitted = true}>Submit</button>
+                        <button type="submit" name="submit" class="btn btn-info" value="submit" on:click|preventDefault={handleSubmit}>Submit</button>
                     </div>
-                </form>
+                </div>
             </div>
         </div>
     </div>
@@ -34,10 +49,14 @@
 
 
 <style>
+    p{
+        color:#c00
+    }
     #login{
         background-image: url("login.jpg");
         background-size: cover;
     }
+
     #login .container #login-row #login-box {
         margin-top: 120px;
         max-width: 600px;
@@ -46,15 +65,17 @@
         background-color: #EAEAEA;
         border-radius: 3%;
     }
+
     #login .container #login-row #login-box #login-form {
-        padding: 20px;
+        padding: 17px;
     }
 
-    .submitted input:focus:invalid {
+    .sub input:focus:invalid {
 		outline: 1px solid #c00;
 	}
 
-    .submitted input:invalid {
+    .sub input:invalid {
 		border: 1px solid #c00;
 	}
+
 </style>
